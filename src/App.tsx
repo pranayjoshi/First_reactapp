64eeee28f04f1093ce6b18a1f95cfa641f36696d
pranayjoshi <pranayjoshi4466@gmail.com>
2023-03-24 10:54:33 +0530

    Created

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import './App.css'
+import ListGroup from './components/ListGroup';
 import Message from './Message'
 
 function App() {
-  return <div><Message></Message></div>;
+  return <div>
+    <Message></Message>
+    <ListGroup/>
+    </div>;
 }
 
 export default App;
---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,18 @@
+ 
+
+
+function ListGroup(){
+    return (
+    <>
+    <ul className="list-group">
+        <li className="list-item">Hi My</li>
+        <li className="list-item">Hi My</li>
+        <li className="list-item">Hi My</li>
+        <li className="list-item">Hi My</li>
+        <li className="list-item">Hi My</li>
+    </ul>
+    </>
+    )
+}
+
+export default ListGroup;
